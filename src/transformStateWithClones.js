b35cfa61f30ceeba08218582c758cf670a27d336
Liena Grytsyna <grytsynaliena@gmail.com>
2025-11-11 12:44:11 +0100

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -30,6 +30,8 @@ function transformStateWithClones(state, actions) {
         currentState = newState;
         break;
       }
+      default:
+        break;
     }
 
     history.push({ ...currentState });
